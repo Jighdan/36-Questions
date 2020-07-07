bafdede8a4de3b1f114539fcde33038028f27d2f
Reinny <reynsth@gmail.com>
2020-07-06 22:01:29 -0400

    First commit

---
+++ scripts/swiperInitializer.js
@@ -0,0 +1,13 @@
+const mySwiper = new Swiper(".swiper-container", {
+	loop: true,
+
+	pagination: {
+		el: ".swiper-pagination",
+		type: "progressbar"
+	},
+
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev"
+	}
+})
