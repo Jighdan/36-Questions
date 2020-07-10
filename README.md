f174bc295fd59d22afbccfe291fedd4508983cdf
Reinny <reynsth@gmail.com>
2020-07-10 15:30:11 -0400

    final

---
+++ README.md
@@ -0,0 +1,3 @@
+# 36 Questions
+
+These questions are the result of a study conducted by Arthur Aron & Elaine Spaulding intent to answer: _How might we, in a laboratory setting, find a way to create instant intimacy between strangers?_
--- scripts/swiperInitializer.js
+++ scripts/swiperInitializer.js
@@ -3,11 +3,7 @@ const mySwiper = new Swiper(".swiper-container", {
 
 	pagination: {
 		el: ".swiper-pagination",
-		type: "progressbar"
-	},
-
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev"
+		clickable: true,
+		type: "fraction"
 	}
 })
